a3295bdc6d06991f9c1f4e77a7db1ed64ae43119
C02Z66GKLVCG <c02z66gklvcg@Apples-MacBook-Pro.local>
2021-05-12 19:37:26 +0530

    template

--- Mo's algorithm/Mo's algo/Mo's algo/main.cpp
+++ Mo's algorithm/Mo's algo/Mo's algo/main.cpp
@@ -13,18 +13,20 @@
 #define INF lli_MAX
 #define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
 using namespace std;
-unordered_map<int,int>mp;
+unordered_map<int, int>mp;
+
+
 int rn;
 int distinct;
 
 struct query
 {
-    int l,r,q;
+    int l, r, q;
     query()
     {
         l = r = q = -1;
     }
-    query(int l,int r,int q)
+    query(int l, int r, int q)
     {
         this->l = l;
         this->r = r;
@@ -32,48 +34,48 @@ struct query
     }
 };
 
-bool comparator(const query &q1,const query &q2)
+bool comparator(const query &q1, const query &q2)
 {
-    if(q1.l/rn==q2.l/rn)
+    if (q1.l / rn == q2.l / rn)
     {
-        return q1.r<q2.r;
+        return q1.r < q2.r;
     }
-    else return q1.l/rn<q2.l/rn;
+    else return q1.l / rn < q2.l / rn;
 }
 
 void Add(int x)
 {
-    if(mp[x]==0) distinct++;
+    if (mp[x] == 0) distinct++;
     mp[x]++;
 }
 
 void Remove(int x)
 {
     mp[x]--;
-    if(mp[x]==0) distinct--;
+    if (mp[x] == 0) distinct--;
 }
 
-void  adjust(int &prevl,int &prevr,query &que,vector<int>&arr)
+void  adjust(int &prevl, int &prevr, query &que, vector<int>&arr)
 {
-    while(prevl<que.l)
+    while (prevl < que.l)
     {
         Remove(arr[prevl]);
         prevl++;
     }
-    
-    while(prevl>que.l)
+
+    while (prevl > que.l)
     {
         prevl--;
         Add(arr[prevl]);
     }
-    
-    while(prevr<que.r)
+
+    while (prevr < que.r)
     {
         prevr++;
         Add(arr[prevr]);
     }
-    
-    while(prevr>que.r)
+
+    while (prevr > que.r)
     {
         Remove(arr[prevr]);
         prevr--;
@@ -83,47 +85,47 @@ void  adjust(int &prevl,int &prevr,query &que,vector<int>&arr)
 int main()
 {
     fast_io;
-    int n,q;
-    cin>>n>>q;
+    int n, q;
+    cin >> n >> q;
     vector<int>arr(n);
-    for(int i=0;i<n;i++) cin>>arr[i];
-    
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
     vector<query>queries;
     vector<int>ans(q);
-    
+
     rn = sqrt(n) + 1;
-    
-    int l,r;
-    
-    for(int i=0;i<q;i++)
+
+    int l, r;
+
+    for (int i = 0; i < q; i++)
     {
-        cin>>l>>r;
-        queries.pb(query(l-1,r-1,i));
+        cin >> l >> r;
+        queries.pb(query(l - 1, r - 1, i));
     }
-    
-    sort(queries.begin(),queries.end(),comparator);
-    
-    
+
+    sort(queries.begin(), queries.end(), comparator);
+
+
     int prevl, prevr;
     distinct = 0;
-    
+
     prevl = queries[0].l;
     prevr = queries[0].r;
-    
-    for(int i=prevl;i<=prevr;i++)
+
+    for (int i = prevl; i <= prevr; i++)
         Add(arr[i]);
-    
+
     ans[queries[0].q] = distinct;
-    
-    for(int i=1;i<q;i++)
+
+    for (int i = 1; i < q; i++)
     {
-        adjust(prevl,prevr,queries[i],arr);
+        adjust(prevl, prevr, queries[i], arr);
         ans[queries[i].q] = distinct;
     }
-    
-    for(int x:ans) cout<<x<<endl;
-    cout<<endl;
-    
+
+    for (int x : ans) cout << x << endl;
+    cout << endl;
+
 }
 
 
---
+++ template/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<map>
+#include<vector>
+#include<climits>
+#include<queue>
+#include<cmath>
+#include<string.h>
+#include<algorithm>
+#include<unordered_map>
+#define pb push_back
+#define N (lli)(1e6+5)
+#define M (lli)(1e6)
+#define lli long long int
+#define mod (lli)(998244353)
+#define INF LLONG_MAX
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define pb push_back
+using namespace std;
+
+
+int  main()
+{
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+
+
+}
